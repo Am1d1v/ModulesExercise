40f50fe1eee37372e447e7119fa1ad513b5ee3f4
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-27 00:04:19 +0300

    Add files via upload

--- moduleOne.mjs
+++ moduleOne.mjs
@@ -10,6 +10,40 @@ export default myName
 
 */
 
+/*
+
 const sum = (a, b) => a + b;
 
 export default sum
+
+*/
+
+/*
+
+const one = 1;
+const two = "two";
+
+export{
+    one,
+    two
+}
+
+*/
+
+/*
+
+const one = "one";
+const two = 2;
+
+export{one, two}
+
+*/
+
+const sum = (a, b) => a + b
+
+const mult = (a, b) => a * b
+
+export {
+    sum,
+    mult
+}
--- moduleTwo.mjs
+++ moduleTwo.mjs
@@ -1,5 +1,5 @@
 
-import sum from './moduleOne.mjs'
+//import sum from './moduleOne.mjs'
 
 /*
 
@@ -19,9 +19,48 @@ console.log(res);
 
 */
 
+/*
+
 const firstArg = +prompt("First Arguement"),
       secondArg = +prompt("Second Arguement");
 
 const res = sum(firstArg, secondArg);
 
 console.log(res); 
+
+*/
+
+/*
+
+import {
+    one,
+    two
+} from './moduleOne.mjs'
+
+console.log(one);
+console.log(two);
+
+*/
+
+/*
+
+import {
+    one as oneRenamed,
+    two
+} from './moduleOne.mjs'
+
+console.log(oneRenamed);
+console.log(two);
+
+*/
+
+import {
+    sum,
+    mult
+} from './moduleOne.mjs'
+
+const sumResult = sum(10,6);
+const multResult = mult(10, 10);
+
+console.log(sumResult);
+console.log(multResult);
